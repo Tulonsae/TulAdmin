813b0a69bd5b146e9f98eb497cb5dc41cb8b8cc9
Tulonsae <admin@tulonsae.org>
2012-08-21 22:36:18 -0700

    Initial version. Console execution doesn't work.

---
+++ src/main/java/org/tulonsae/mc/plugins/TulAdmin.java
@@ -0,0 +1,55 @@
+package org.tulonsae.mc.plugins;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
+
+/**
+ * Base Bukkit plugin class for TulAdmin.
+ * <p />
+ * TulAdmin contains a number of commands used for preparing Minecraft worlds
+ * for play.  These are particularly dangerous to existing worlds so it is
+ * recommended to only enable this plugin when preparing a world.
+ *
+ * @author Tulonsae
+ */
+public class TulAdmin extends JavaPlugin {
+
+    public String version;
+
+    /**
+     * Called when this plugin is enabled.
+     * Loads the configuration; registers for Bukkit events.
+     */
+    public void onEnable() {
+
+        // get plugin info
+        version = this.getDescription().getVersion();
+
+        // register commands
+        getCommand("makevoidregion").setExecutor(new VoidRegion(this));
+        
+        // log enable message
+        LogUtil.info("version " + version + " enabled.");
+    }
+
+    /**
+     * Called when this plugin is disabled.
+     */
+    public void onDisable() {
+        
+        // log disable message
+        LogUtil.info("version " + version + " disabled.");
+    }
+}
+
+/*
+        // generate regions
+        if (cmd.getName().equalsIgnoreCase("generateregion")) {
+            if (args.length < 2) {
+                return false;
+            }
+        }
+*/
